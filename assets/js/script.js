00a4a3cd711ac64251bf46e662191e5a54510c92
gunjan1909 <bhanarkaranita@gmail.com>
2021-09-17 11:58:33 +0530

    select button, scrollbar color

--- assets/js/script.js
+++ assets/js/script.js
@@ -161,6 +161,8 @@ window.onload = function () {
 // 1. Store all the data to the localStorage
 // 2. Give another column as an option to delete the book
 // 3. Add a scroll bar to the view.
+
+//preloader
 let timer1;
 let preloader = document.querySelector(".preloader");
 let nav = document.querySelector(".navbar");
