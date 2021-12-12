7a2df55eece011b368632237edf94bac7fcfe261
Saurav Mukherjee <72380920+SauravMukherjee44@users.noreply.github.com>
2021-12-12 20:33:37 +0530

    Voice Assistant Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -281,3 +281,14 @@ function checkScroll(){
     });
   }
 }
+//Chat-Bot Added
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/61b60cc480b2296cfdd150ce/1fmni3o8e';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
