99ea8777c4f40381f7dc109d76152d9cda04c7dc
shubhanshurav <shubhanshurav63@gmail.com>
2023-06-05 12:49:07 +0530

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ var TxtType = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
+  this.period = parseInt(period, 10) || 2000; 
   this.txt = "";
   this.tick();
   this.isDeleting = false;
