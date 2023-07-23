1b369fc33c5b225f56f768461560055833373f8c
Harshit <harshitpant134@gmail.com>
2023-07-23 11:53:34 +0530

    <#1444 Adding Prettier >

--- Quiz/js/highscore.js
+++ Quiz/js/highscore.js
@@ -2,7 +2,7 @@ const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 highScoresList.innerHTML = highScores
-    .map(score => {
-        return `<li class="high-score">${score.name} - ${score.score}</li>`;
-    })
-    .join("");
+  .map((score) => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  })
+  .join("");
